b56f6fee51eed427e35e3dccf26d91c1b8211543
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-11-03 00:19:41 +0200

    fix: pass `--no-sandbox` to Puppeteer in GitHub Actions

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -5,6 +5,7 @@ import { execSync } from 'child_process';
 import { describe, it, expect, beforeAll, afterAll } from 'vitest';
 
 const CLI_PATH = path.resolve('./build/src/cli.js');
+const DISABLE_SANDBOX = process.env.GITHUB_ACTIONS ? `--puppeteerOptions '{"args":["--no-sandbox"]}'` : '';
 const TEST_OUTPUT_DIR = path.join(os.tmpdir(), 'pdown-test-output');
 
 describe('CLI', () => {
@@ -60,14 +61,14 @@ describe('CLI', () => {
 					}
 				}
 			];
-			const output = execSync(`node ${CLI_PATH} ls KGER0RS624#LzmiMIuikOuj --recursive --json`, { encoding: 'utf8' });
+			const output = execSync(`node ${CLI_PATH} ls KGER0RS624#LzmiMIuikOuj --recursive --json ${DISABLE_SANDBOX}`, { encoding: 'utf8' });
 			expect(JSON.parse(JSON.parse(output).message)).toEqual(expected);
 		});
 	});
 
 	describe('dl', () => {
 		it('should be able to download password protected shares', () => {
-			execSync(`node ${CLI_PATH} Y5J2AT9QJ0#HjVxIlCjfd99 --password love --output ${TEST_OUTPUT_DIR}`, { encoding: 'utf8' });
+			execSync(`node ${CLI_PATH} Y5J2AT9QJ0#HjVxIlCjfd99 --password love --output ${TEST_OUTPUT_DIR} ${DISABLE_SANDBOX}`, { encoding: 'utf8' });
 
 			const files = fs.readdirSync(TEST_OUTPUT_DIR);
 			expect(files.length).toBeGreaterThan(0);
